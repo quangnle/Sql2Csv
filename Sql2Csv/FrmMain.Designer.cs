2e0999db4fa4f44108d2d6b21c1071c383fa1287
quangnle <mnest2409@yahoo.com>
2017-01-13 10:36:53 +0800

    commit

---
+++ Sql2Csv/FrmMain.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Sql2Csv
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.grdData = new System.Windows.Forms.DataGridView();
+            this.lblNRow = new System.Windows.Forms.Label();
+            this.btnExportToCSV = new System.Windows.Forms.Button();
+            this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(943, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(48, 12);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 20);
+            this.txtServer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Database";
+            // 
+            // txtDb
+            // 
+            this.txtDb.Location = new System.Drawing.Point(218, 12);
+            this.txtDb.Name = "txtDb";
+            this.txtDb.Size = new System.Drawing.Size(100, 20);
+            this.txtDb.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(690, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Login";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(731, 12);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 5;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(837, 12);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 7;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Enabled = false;
+            this.txtQuery.Location = new System.Drawing.Point(10, 38);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(927, 84);
+            this.txtQuery.TabIndex = 8;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Enabled = false;
+            this.btnExecute.Location = new System.Drawing.Point(943, 36);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 86);
+            this.btnExecute.TabIndex = 9;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // grdData
+            // 
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Location = new System.Drawing.Point(12, 128);
+            this.grdData.Name = "grdData";
+            this.grdData.Size = new System.Drawing.Size(1006, 329);
+            this.grdData.TabIndex = 10;
+            // 
+            // lblNRow
+            // 
+            this.lblNRow.AutoSize = true;
+            this.lblNRow.Location = new System.Drawing.Point(7, 469);
+            this.lblNRow.Name = "lblNRow";
+            this.lblNRow.Size = new System.Drawing.Size(78, 13);
+            this.lblNRow.TabIndex = 11;
+            this.lblNRow.Text = "Row Count = 0";
+            // 
+            // btnExportToCSV
+            // 
+            this.btnExportToCSV.Location = new System.Drawing.Point(943, 463);
+            this.btnExportToCSV.Name = "btnExportToCSV";
+            this.btnExportToCSV.Size = new System.Drawing.Size(75, 25);
+            this.btnExportToCSV.TabIndex = 12;
+            this.btnExportToCSV.Text = "To CSV";
+            this.btnExportToCSV.UseVisualStyleBackColor = true;
+            this.btnExportToCSV.Click += new System.EventHandler(this.btnExportToCSV_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 491);
+            this.Controls.Add(this.btnExportToCSV);
+            this.Controls.Add(this.lblNRow);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.btnConnect);
+            this.MaximumSize = new System.Drawing.Size(1046, 529);
+            this.MinimumSize = new System.Drawing.Size(1046, 529);
+            this.Name = "FrmMain";
+            this.Text = "Export SQL to CSV";
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.DataGridView grdData;
+        private System.Windows.Forms.Label lblNRow;
+        private System.Windows.Forms.Button btnExportToCSV;
+        private System.Windows.Forms.SaveFileDialog dlgSaveFile;
+    }
+}
+
---
+++ Sql2Csv/FrmMain.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sql2Csv
+{
+    public partial class FrmMain : Form
+    {
+
+        private SqlConnection _con = null;
+        private string configFile = "config.dat";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            var path = Directory.GetCurrentDirectory() + @"\" + configFile;
+            if (File.Exists(path))
+            {
+                var cfg = File.ReadAllLines(configFile);
+                txtServer.Text = cfg[0];
+                txtDb.Text = cfg[1];
+                txtUser.Text = cfg[2];
+                txtPwd.Text = cfg[3];
+            }
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {   
+            _con = new SqlConnection();
+            var conStr = "Data Source={0};Initial Catalog={1};User Id={2}; Password = {3};";
+            _con.ConnectionString = string.Format(conStr, txtServer.Text, txtDb.Text, txtUser.Text, txtPwd.Text);
+
+            try
+            {
+                _con.Open();
+                MessageBox.Show("Connection is successfully established.");
+
+                txtQuery.Enabled = true;
+                btnExecute.Enabled = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Cannot establish connection to server.");
+            }
+            finally
+            {
+                _con.Close();
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(txtQuery.Text);
+                cmd.Connection = _con;
+
+                _con.Open();
+
+                var reader = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                grdData.DataSource = dt;
+                grdData.Refresh();
+
+                lblNRow.Text = "Row Count = " + dt.Rows.Count.ToString();
+
+                _con.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _con.Close();
+            }
+            
+        }
+
+        private void btnExportToCSV_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dlgSaveFile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var fileName = dlgSaveFile.FileName;
+                var dt = grdData.DataSource as DataTable;
+                StringBuilder sb = new StringBuilder();
+
+                IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                                  Select(column => column.ColumnName);
+                sb.AppendLine(string.Join(",", columnNames));
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+                File.WriteAllText(fileName, sb.ToString());
+            }
+        }
+    }
+}
